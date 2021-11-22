9706870ce99e967cc142531a3c55a11627cf9688
Matt-DWJ <matthieu.junge@outlook.com>
2021-11-22 10:55:19 +0100

    Update nav.js
    fix

--- js/nav.js
+++ js/nav.js
@@ -1,4 +1,21 @@
 $('document').ready(function () {
 
+    //Menu hamburger au Clic !
+
+    let buttonHamburger = document.getElementById("icon");
+    let menu = document.getElementById("main-nav");
+
+    buttonHamburger.addEventListener('click', function (e) {
+        menu.classList.toggle('d-none');
+    });
+
+    //Menu pays au Clic !
+
+    let iconPays = document.getElementById("other");
+    let iconFr = document.getElementById("pays");
+
+    iconFr.addEventListener('click', function () {
+        iconPays.classList.toggle('d-none');
+    });
 
 });
