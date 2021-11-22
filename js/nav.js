d4ff5d3b6a931d5703bac98391ce9e3ad0ded9e4
Matt-DWJ <matthieu.junge@outlook.com>
2021-11-22 10:47:39 +0100

    fix
    fix

--- js/nav.js
+++ js/nav.js
@@ -1,33 +1,4 @@
-$('document').ready(function() {
+$('document').ready(function () {
 
-    //Menu hamburger au Clic !
-
-    let buttonHamburger = document.getElementById("icon");
-    let menu = document.getElementById("main-nav");
-
-    buttonHamburger.addEventListener('click', function (e)
-    {
-        if (menu.style.display="none"){
-            menu.style.display="grid"
-        } else {
-            menu.style.display="none";
-        }
-    });
-
-
-    //Menu pays au Clic !
-    $("#pays").click(function () {
-
-        let iconPays = document.getElementById("other");
-        let iconFr = document.getElementById("pays");
-        if (iconPays.style.display === "block") {
-            iconPays.style.display = "none";
-            iconFr.style.border = "";
-            iconFr.style.borderRadius = "";
-        } else {
-            iconPays.style.display = "block";
-
-        }
-    });
 
 });
